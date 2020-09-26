3e4b854e73888deac32c020a6d77e012e38dbc1c
Andre Ferrer <andrerferrer@hotmail.com>
2020-09-26 06:25:40 -0300

    Improve the boilerplate

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-// We will add a button that will
+// We will add an event listener to a button that will
 // fetch from https://lw-rio-quotes.herokuapp.com/api/v1/quotes/milene
 // and update the DOM
